c63e2ab7f7a35a214296a31c10ae1bb80aa14228
Briana Dodgen <brianadodgen@gmail.com>
2022-10-03 13:08:16 +0200

    React intro done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>The demons will eat ur sooouuuulllll, h1!</h2>
+  )
+}
+
+function App() {
+  const amazingClassName = "myApp";
+  const badassClassName = "someoneElsesApp";
+
+  return (
+    <div className={amazingClassName + " " + badassClassName}>
+      <h1>AAAAAHHHHHHHH! HELP MEEEEE! I'M TRAPPED IN THE POOTER! SOS</h1>
+      {Hello()}
     </div>
   );
 }
